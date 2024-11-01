eb11699da53c4c6a240665e9949bf854cf074542
imwood04 <redredga829@gmail.com>
2024-11-01 06:43:10 -0400

    .....

--- Practice Files/Lists/list.py
+++ Practice Files/Lists/list.py
@@ -1,8 +1,8 @@
 seats = [
-['a', 'b', 'c'],
-['d', 'e', 'f'],
-['g', 'h', 'i'],
-['j', 'k', 'l']
+    ['a', 'b', 'c'],
+    ['d', 'e', 'f'],
+    ['g', 'h', 'i'],
+    ['j', 'k', 'l']
 ]
 row = int(input())
 column = int(input())
