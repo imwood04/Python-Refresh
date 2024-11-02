0b55c4bfa52fb19c1d8ba0bd8106a92fe994fea8
imwood04 <redredga829@gmail.com>
2024-11-02 02:30:05 -0400

    StringsLists.py

---
+++ Practice Files/Lists/StringsLists.py
@@ -0,0 +1,13 @@
+# You need to take the first and last name of a person as input and output the initials (first letters of the first and last name).
+#
+# Sample Input
+# James
+# Smith
+#
+# Sample Output
+# J.S.
+
+fname = input()
+lname = input()
+
+print(f"{fname[0]}.{lname[0]}.")
