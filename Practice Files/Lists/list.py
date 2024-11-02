38c4e90bc12e50f4e0b0ab7ce1c2f93d44be31ee
imwood04 <redredga829@gmail.com>
2024-11-02 02:29:54 -0400

    StringsLists.py

--- Practice Files/Lists/list.py
+++ Practice Files/Lists/list.py
@@ -1,3 +1,14 @@
+# Lists
+# The seats in your ticketing program are stored in a 2D list. Each seat is assigned a letter code.
+#
+# Complete the program to take the seat row and column as input and output the corresponding code from the list (row and column indices start from 0).
+#
+# Sample Input
+# 3
+# 2
+#
+# Sample Output
+# l
 seats = [
     ['a', 'b', 'c'],
     ['d', 'e', 'f'],
