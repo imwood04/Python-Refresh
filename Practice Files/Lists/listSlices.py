24547e8d7224e174a228fd246472a8fefa54d2a5
imwood04 <redredgav829@gmail.com>
2024-11-03 21:07:17 -0500

    listSlices.py

--- Practice Files/Lists/listSlices.py
+++ Practice Files/Lists/listSlices.py
@@ -1,1 +1,13 @@
-l
+# Reversing a string is a common task during coding interviews.
+#
+# Given a string as input, output its reverse.
+#
+# Sample Input
+# hello world
+#
+# Sample Output
+# dlrow olleh
+
+listInput = [input()]
+
+print(list[0][::-1])
