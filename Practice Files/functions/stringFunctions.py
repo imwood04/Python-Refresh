860e593b62e7cde883ca47894b68027eeedc0d52
imwood04 <redredgav829@gmail.com>
2024-11-04 07:31:18 -0500

    Added stringFunctions.py

---
+++ Practice Files/functions/stringFunctions.py
@@ -0,0 +1,7 @@
+# Your friend is sending you a text message, however his caps lock is broken and the whole message is in uppercase.
+# Noone likes being shouted at, plus uppercase text is hard to read, so you decide to write a program to convert the text to lowercase and output it.
+#
+# Take a string as input and output it in lowercase.
+
+text = input()
+print(text.lower())
