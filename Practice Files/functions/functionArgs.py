639014e347d16046e499c408df8239ddd5e31972
imwood04 <redredgav829@gmail.com>
2024-11-06 03:06:10 -0500

    Added functionArgs.py

---
+++ Practice Files/functions/functionArgs.py
@@ -0,0 +1,14 @@
+# You need to make a function that converts a foot value to inches.
+#
+# 1 foot has 12 inches.
+#
+# Define a convert() function, that takes the foot value as argument and outputs the inches value.
+
+feet = int(input())
+
+
+def convert(inch):
+    print(feet * 12)
+
+
+convert(feet)
